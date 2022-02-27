6f082bf03b04516061b1f0e4740ff7d2154ed493
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2022-02-27 06:36:37 +0100

    recomended playlist on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,17 @@
         <router-link class="abt" to = "/about">
             <h2 href = "About.vue">I make stuff</h2>
         </router-link>
+
+        <div class="play">
+            <h2>Recomended Playlist</h2>
+            <iframe 
+            width="560" height="315" 
+            src="https://www.youtube.com/embed/videoseries?list=PLMSpkqIzr6oss-BSeVn0Mb85UO2Va3Xkm" 
+            title="Recomended Playlist" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+            </iframe>
+        </div>
     </main>
 </template>
 
@@ -33,10 +44,17 @@
         margin-top: 1rem;
     }
 
+    .play {
+        display: grid;
+        place-items: center;
+        margin-top: 2rem;
+    }
+
     .home-page {
         @media (max-width: 1024px) {
 			padding-left: 6rem;
 		}
     }
+    
 
 </style>

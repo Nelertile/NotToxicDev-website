46684d2bd981064bd5828dc762083df6cb691968
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2022-02-28 19:50:54 +0100

    add discord

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -6,6 +6,7 @@
             <a href="https://discord.io/vanillaextract" target="_blank" class="vanillaExtract">| Vanilla Extract |</a>
             <a href="https://bit.ly/ntgyt" target="_blank" class="youtube"><i class="fab fa-youtube"></i> YouTube |</a>
             <a href="https://github.com/NotToxicDev" class="github"><i class="fab fa-github"></i> GitHub |</a>
+            <a href="https://discord.gg/GNRC2Gu5Yb" class="discord"><i class="fab fa-discord"></i> Discord |</a>
        </div>
     </main>
 </template>
